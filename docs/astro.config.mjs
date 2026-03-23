24c386276142ccf309541b649b79df527c7215d5
Martin Bonnin <martin@mbonnin.net>
2026-03-23 15:23:56 +0100

    lowercase

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 export default defineConfig({
 	site: 'https://gradleup.com',
-	base: '/Tapmoc',
+	base: '/tapmoc',
 	integrations: [
 		starlight({
 			title: 'Tapmoc',
