4cf804ca33bbc97c0622c535a002d77f7dfb8b70
Martin Bonnin <martin@mbonnin.net>
2026-03-30 23:44:52 +0200

    Version is now 0.4.2-SNAPSHOT

--- scripts/update-repo.main.kts
+++ scripts/update-repo.main.kts
@@ -3,14 +3,16 @@
 @file:Repository("https://repo.maven.apache.org/maven2/")
 @file:Repository("https://dl.google.com/android/maven2/")
 @file:Repository("https://storage.googleapis.com/gradleup/m2")
-@file:Repository("https://jitpack.io")
 //@file:Repository("file://~/.m2/repository")
-@file:DependsOn("com.gradleup.librarian:librarian-cli:0.2.2-SNAPSHOT-0cbca80f60a15a0f851a9cd468bfc352db316dd4")
+@file:DependsOn("com.gradleup.librarian:librarian-cli:0.2.2-SNAPSHOT-b89fe292839b757bf152e8369a88991052d32d0b")
 
-import com.gradleup.librarian.cli.updateRepo
+import com.gradleup.librarian.repo.updateRepo
 
-updateRepo(args) {
-  file("README.md") {
-    replacePluginVersion("com.gradleup.tapmoc")
-  }
-}
+
+updateRepo(
+  setVersionInDocs = {
+    file("README.md") {
+      replacePluginVersion("com.gradleup.tapmoc")
+    }
+  },
+)
